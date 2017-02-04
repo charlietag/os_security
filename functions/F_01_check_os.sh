044553490597f81c4b48f317f9bd32915cc24665
Charlie Hsieh <charlieofficial@gmail.com>
2017-02-04 09:30:47 +0800

    change var name

--- functions/F_01_check_os.sh
+++ functions/F_01_check_os.sh
@@ -5,15 +5,15 @@
 
 for os_command in ${os_commands[@]}
 do
-  COMM="$(which $os_command)"
-  echo "===${COMM}==="
-  BUGS="$(rpm -Vf $COMM)"
-  if [ -z "${BUGS}" ]
+  local command_dir="$(which $os_command)"
+  echo "===${command_dir}==="
+  check_result="$(rpm -Vf $command_dir)"
+  if [ -z "${check_result}" ]
   then
     echo "PASS"
   else
-    echo "Command \"${COMM}\" is hacked"
-    echo "${BUGS}"
+    echo "Command \"${command_dir}\" is hacked"
+    echo "${check_result}"
   fi
   echo ""
 done
