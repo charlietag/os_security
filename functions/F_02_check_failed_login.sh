8d9eb5f3b698cb966ec2298171aa58329a6e70e3
Charlie Hsieh <charlieofficial@gmail.com>
2017-02-06 07:34:46 +0800

    mv log file to databag

--- functions/F_02_check_failed_login.sh
+++ functions/F_02_check_failed_login.sh
@@ -1,2 +1,8 @@
-cat /var/log/secure* | grep "Failed" |grep    "invalid"  | awk '{print $11}' | sort  |uniq -c
-cat /var/log/secure* | grep "Failed" |grep -v "invalid"  | awk '{print $9}'  | sort  |uniq -c
+# =====================
+# Enable databag
+# =====================
+# RENDER_CP
+
+# ssh failed login check
+cat $ssh_log_file | grep "Failed" |grep    "invalid"  | awk '{print $11}' | sort  |uniq -c
+cat $ssh_log_file | grep "Failed" |grep -v "invalid"  | awk '{print $9}'  | sort  |uniq -c
