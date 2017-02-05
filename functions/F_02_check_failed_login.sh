4cc4a67cd3a2b96bb753ff23c1039e67e3cbe497
Charlie Hsieh <charlieofficial@gmail.com>
2017-02-05 16:51:43 +0800

    fix column issue

--- functions/F_02_check_failed_login.sh
+++ functions/F_02_check_failed_login.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat /var/log/secure* | grep "Failed" |grep    "root"  | awk '{print $9}' | sort  |uniq -c
-cat /var/log/secure* | grep "Failed" |grep -v "root"  | awk '{print $11}'| sort  |uniq -c
+cat /var/log/secure* | grep "Failed" |grep    "invalid"  | awk '{print $11}' | sort  |uniq -c
+cat /var/log/secure* | grep "Failed" |grep -v "invalid"  | awk '{print $9}'  | sort  |uniq -c
