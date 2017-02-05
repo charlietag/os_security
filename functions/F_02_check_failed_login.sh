3e15fd14522d4900468dac79f18dfb3db7decb76
Charlie Hsieh <charlieofficial@gmail.com>
2017-02-05 16:35:18 +0800

    fix column

--- functions/F_02_check_failed_login.sh
+++ functions/F_02_check_failed_login.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-grep Failed /var/log/secure |cut -d' ' -f9| sort  |uniq -c
+cat /var/log/secure* | grep Failed |grep root  |cut -d' ' -f9| sort  |uniq -c
+cat /var/log/secure* | grep Failed |grep -v root  |cut -d' ' -f11| sort  |uniq -c
