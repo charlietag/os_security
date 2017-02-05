cbad786fae84a4cdb3e1ba8b01a21a9f1ca3f79d
Charlie Hsieh <charlieofficial@gmail.com>
2017-02-05 20:01:21 +0800

    remove header line

--- functions/F_02_check_failed_login.sh
+++ functions/F_02_check_failed_login.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
 cat /var/log/secure* | grep "Failed" |grep    "invalid"  | awk '{print $11}' | sort  |uniq -c
 cat /var/log/secure* | grep "Failed" |grep -v "invalid"  | awk '{print $9}'  | sort  |uniq -c
