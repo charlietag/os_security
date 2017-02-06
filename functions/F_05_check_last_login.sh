38d6ff7e898dddafd3101fe21717a3c74a03a572
Charlie Hsieh <charlieofficial@gmail.com>
2017-02-06 08:24:11 +0800

    fix error

--- functions/F_05_check_last_login.sh
+++ functions/F_05_check_last_login.sh
@@ -8,6 +8,6 @@ l=$(grep "^UID_MIN" $_l)
 l1=$(grep "^UID_MAX" $_l)
  
 ## use awk to print if UID >= $MIN and UID <= $MAX and shell is not /sbin/nologin   ##
-local normal_user="$(awk -F':' -v "min=${l##UID_MIN}" -v "max=${l1##UID_MAX}" '{ if ( $3 >= min && $3 <= max  && $7 != "/sbin/nologin" ) print $0 }' "$_p" |cut -d':' -f1 |tr '\n' '|')"
+local normal_user="$(awk -F':' -v "min=${l##UID_MIN}" -v "max=${l1##UID_MAX}" '{ if ( $3 >= min && $3 <= max  && $7 != "/sbin/nologin" ) print $0 }' "$_p" |cut -d':' -f1 |tr '\n' '|' | sed 's/.$//' )"
 
 last |grep -Ev "${normal_user}"
