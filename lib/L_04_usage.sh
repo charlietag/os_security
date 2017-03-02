3552463a214e312e7492b8c46bd5f8969cb9e9fc
Charlie Hsieh <charlieofficial@gmail.com>
2017-03-03 01:08:08 +0800

    change core lib, to gain elegant help message, to add check template before rendering

--- lib/L_04_usage.sh
+++ lib/L_04_usage.sh
@@ -5,7 +5,7 @@ L_PRINT_HELP (){
   echo "usage: $(basename "${CURRENT_SCRIPT}")"
   echo "  -a                   ,  run all functions"
   echo "  -i func1 func2 func3 ,  run specified functions"
-  echo "                          example: $(basename "${CURRENT_SCRIPT}") -i ${FUNC_NAMES[@]}"
+  echo -e "                          example: $(basename "${CURRENT_SCRIPT}") -i \\\\\n$(echo "${FUNC_NAMES[@]}" | sed 's/\ /\ \\\\\n/g')"
   exit
 }
 
