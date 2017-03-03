df14cc2b6d7fd5f9eda007e6c3720c57a035bee8
Charlie Hsieh <charlieofficial@gmail.com>
2017-03-03 20:58:56 +0800

    check failed login via command lastb

--- functions/F_02_check_failed_login.sh
+++ functions/F_02_check_failed_login.sh
@@ -4,5 +4,13 @@
 # RENDER_CP
 
 # ssh failed login check
+echo "-----------------------"
+echo "check via ssh log file"
+echo "-----------------------"
 cat $ssh_log_file | grep "Failed" |grep    "invalid"  | awk '{print $11}' | sort  |uniq -c
 cat $ssh_log_file | grep "Failed" |grep -v "invalid"  | awk '{print $9}'  | sort  |uniq -c
+
+echo "-----------------------"
+echo "check via command lastb"
+echo "-----------------------"
+lastb
