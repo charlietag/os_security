c505550aea943bb2ddb1ea998a5bd6e666845aa7
Charlie Hsieh <charlieofficial@gmail.com>
2017-03-26 16:44:01 +0800

    add config into log analyzer

--- functions/F_11_install_log_analyzer.sh
+++ functions/F_11_install_log_analyzer.sh
@@ -1,2 +1,28 @@
+# ***************************
+# logwatch
+# ***************************
 yum install -y logwatch
+
+# ***************************
+# logwatch
+# ***************************
 yum install -y goaccess
+
+# ***************************
+# setup config
+# ***************************
+echo "========================================="
+echo "  Setup configs"
+echo "========================================="
+local log_analyzer_confs=($(find ${CONFIG_FOLDER} -type f))
+local log_analyzer_target=""
+local log_analyzer_target_folder=""
+for log_analyzer_conf in ${log_analyzer_confs[@]}
+do
+  log_analyzer_target="${log_analyzer_conf/${CONFIG_FOLDER}/}"
+  log_analyzer_target_folder="$(dirname $log_analyzer_target)"
+
+  test -d $log_analyzer_target_folder || mkdir -p $log_analyzer_target_folder
+  echo "Setting up config file \"${log_analyzer_target}\"......"
+  \cp -a --backup=t $log_analyzer_conf $log_analyzer_target
+done
