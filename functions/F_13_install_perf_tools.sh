e9403115b9239b5aedee4b7000bebbf4155be85a
Charlie Hsieh <charlieofficial@gmail.com>
2017-03-26 16:59:05 +0800

    add mytop config render

--- functions/F_13_install_perf_tools.sh
+++ functions/F_13_install_perf_tools.sh
@@ -1,2 +1,28 @@
+# ***************************
+# glances
+# ***************************
 yum install -y glances
+
+# ***************************
+# mytop - mysql perf monitor
+# ***************************
 yum install -y mytop
+
+# ***************************
+# setup config
+# ***************************
+echo "========================================="
+echo "  Setup configs"
+echo "========================================="
+local perf_tools_confs=($(find ${CONFIG_FOLDER} -type f))
+local perf_tools_target=""
+local perf_tools_target_folder=""
+for perf_tools_conf in ${perf_tools_confs[@]}
+do
+  perf_tools_target="${perf_tools_conf/${CONFIG_FOLDER}/}"
+  perf_tools_target_folder="$(dirname $perf_tools_target)"
+
+  test -d $perf_tools_target_folder || mkdir -p $perf_tools_target_folder
+  # use RENDER_CP to fetch var from datadog
+  RENDER_CP $perf_tools_conf $perf_tools_target
+done
