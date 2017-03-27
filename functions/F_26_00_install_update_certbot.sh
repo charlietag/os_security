e5289c1671d2228c8757823cb1f70dcc7c670ac6
Charlie Hsieh <charlieofficial@gmail.com>
2017-03-27 23:08:41 +0800

    add renew script

--- functions/F_26_00_install_update_certbot.sh
+++ functions/F_26_00_install_update_certbot.sh
@@ -23,9 +23,32 @@ fi
 echo "disable httpd server..."
 systemctl disable httpd
 
-# *********************************
-# Adding certificates renewal into crontab
-# *********************************
-echo "Adding cert renewal into crontab..."
-sed -i /certbot-auto/d /etc/crontab
-echo "1 3 * * 1 root ${certbot_command} renew -n -q ; systemctl restart optnginx" >> /etc/crontab
+# ******* Install certbot renew script *******
+echo "========================================="
+echo "   Install certbot renew script"
+echo "========================================="
+local file_certbot_confs=($(find ${CONFIG_FOLDER} -type f))
+local file_certbot_target=""
+local file_certbot_target_folder=""
+for file_certbot_conf in ${file_certbot_confs[@]}
+do
+  file_certbot_target="${file_certbot_conf/${CONFIG_FOLDER}/}"
+  file_certbot_target_folder="$(dirname $file_certbot_target)"
+
+  test -d $file_certbot_target_folder || mkdir -p $file_certbot_target_folder
+  echo "Setting up config file \"${file_certbot_target}\"......"
+  \cp -a --backup=t $file_certbot_conf $file_certbot_target
+
+  # ********* Only Chmod certbot-auto_renew  ********
+  if [ "$(basename $file_certbot_target)" = "certbot-auto_renew" ]
+  then
+    chmod 777 $file_certbot_target
+    # *********************************
+    # Adding certificates renewal into crontab
+    # *********************************
+    echo "Adding cert renewal into crontab..."
+    sed -i /certbot-auto/d /etc/crontab
+    echo "1 3 * * * root ${file_certbot_target}" >> /etc/crontab
+  fi
+done
+
