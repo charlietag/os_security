d0b050f232512b80a01e777998098b8fa8e538bd
Charlie Hsieh <charlieofficial@gmail.com>
2017-05-29 11:39:02 +0800

    add letsencrypt - webroot support

---
+++ functions/F_26_01_certbot_apply_webroot.sh
@@ -0,0 +1,22 @@
+#**********************************************
+# =====================
+# Enable databag
+# =====================
+# RENDER_CP
+
+# Init action
+. ${FUNCTIONS}/_certbot_init.sh
+# Before action
+. ${FUNCTIONS}/_certbot_before.sh
+#**********************************************
+
+#**********************************************
+# Start to apply letsencrypt SSL cert with WEBROOT verification
+echo "---Apply cert using certbot via webroot---"
+$certbot_command --agree-tos -m $certbot_email --no-eff-email certonly --webroot -w $certbot_webroot -d $certbot_servername -n -q
+#**********************************************
+
+#**********************************************
+# After action
+. ${FUNCTIONS}/_certbot_after.sh
+#**********************************************
