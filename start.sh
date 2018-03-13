963672119b56214cb1b5d62c6380704616c1952c
Charlie Hsieh <charlieofficial@gmail.com>
2018-03-13 22:33:09 +0800

    add more verification for folder os_preparation_lib

--- start.sh
+++ start.sh
@@ -15,16 +15,32 @@ echo "#############################################"
 echo "         Preparing required lib"
 echo "#############################################"
 OS_PRE_LIB="${CURRENT_FOLDER}/../os_preparation_lib"
+
+# ### Make sure os_preparation_lib exists correctly ###
+RC=1
 if [[ ! -d "${OS_PRE_LIB}" ]]; then
   cd "$CURRENT_FOLDER/../"
   echo "Downloading required lib..."
   git clone $OS_PRE_LIB_GITHUB
+  RC=$?
 else
   cd $OS_PRE_LIB
   echo "Updating required lib to lastest version..."
   git pull
+  RC=$?
+fi
+
+if [[ $RC -ne 0 ]]; then
+  echo "Error occurs fetching github... !"
+  exit
+fi
+
+if [[ ! -d "${OS_PRE_LIB}" ]]; then
+  echo "${OS_PRE_LIB} does not exist... !"
+  exit
 fi
 echo ""
+# ### Make sure os_preparation_lib exists correctly ###
 
 # --- Define lib path ---
 ## also in app.sh ##
