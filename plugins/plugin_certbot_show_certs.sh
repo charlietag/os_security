4af74c7a09852753dfd89c2186312962ca3358f0
Dev Use <dev@gmail.com>
2018-07-19 22:54:47 +0800

    add restart nginx server by default , after all certbot actions

--- plugins/plugin_certbot_show_certs.sh
+++ plugins/plugin_certbot_show_certs.sh
@@ -5,3 +5,7 @@ echo "-----disable httpd server...-----"
 systemctl disable httpd
 systemctl stop httpd
 
+echo "-----restart nginx server to load SSL Cert files...-----"
+systemctl stop nginx ; sleep 3 ; systemctl start nginx
+
+echo "----- Finished restarting nginx server-----"
