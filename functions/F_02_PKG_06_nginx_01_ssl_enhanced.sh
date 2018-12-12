113b70aa3f404e7b9ff8b16c8d21d05cc38d8164
Charlie Hsieh <charlieofficial@gmail.com>
2018-12-13 07:06:09 +0800

    change structure of nginx plugins

--- functions/F_02_PKG_06_nginx_01_ssl_enhanced.sh
+++ functions/F_02_PKG_06_nginx_01_ssl_enhanced.sh
@@ -5,5 +5,5 @@ echo "Generating openssl dhparam."
 echo "This might take \"several minutes!\""
 echo "Base on your server spec. This might take even \"up to hours\"...!"
 echo "-------------------------------------------------------------"
-local ssl_dhparam_file="/etc/nginx/ssl_dhparam/dhparam2048.pem"
+local ssl_dhparam_file="/etc/nginx/server_features/SSL/ssl_dhparam/dhparam2048.pem"
 test -f $ssl_dhparam_file || $(which openssl) dhparam -out $ssl_dhparam_file 2048
