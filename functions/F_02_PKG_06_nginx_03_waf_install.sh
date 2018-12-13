fdcb753efba511a7554a0915b1a991bc6a842e0e
Charlie Hsieh <charlieofficial@gmail.com>
2018-12-14 07:49:46 +0800

    adding nginx-waf libmod script

--- functions/F_02_PKG_06_nginx_03_waf_install.sh
+++ functions/F_02_PKG_06_nginx_03_waf_install.sh
@@ -5,3 +5,15 @@
 
 # Install libmodsecurity (ModSecurity for Nginx)
 yum install -y libmodsecurity*
+
+# ******* Install Nginx-modsecurity update script *******
+echo "========================================="
+echo "   Setup install update Nginx-modsecurity script"
+echo "========================================="
+task_copy_using_cat
+
+# Build and install ngx_http_modsecurity_module.so 
+git clone --depth 1 -b v1.0.0 https://github.com/SpiderLabs/ModSecurity-nginx.git
+
+
+
