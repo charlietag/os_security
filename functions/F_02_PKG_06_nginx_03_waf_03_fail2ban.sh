8eec469b9309d40c529a62d82187a057c98c6311
Charlie Hsieh <charlieofficial@gmail.com>
2019-01-06 00:10:47 +0800

    add fail2ban for nginx-waf

--- functions/F_02_PKG_06_nginx_03_waf_03_fail2ban.sh
+++ functions/F_02_PKG_06_nginx_03_waf_03_fail2ban.sh
@@ -7,7 +7,7 @@
 echo "========================================="
 echo "   Setup fail2ban for Nginx-modsecurity (Nginx WAF)"
 echo "========================================="
-task_copy_using_cat
+task_copy_using_render
 
 echo "---reload fail2ban---"
 fail2ban-client reload
