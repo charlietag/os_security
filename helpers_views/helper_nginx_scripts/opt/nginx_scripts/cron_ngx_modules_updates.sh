aa189f52882d7ee2c3ed5fb7912b9043c934ed80
Charlie Hsieh <charlieofficial@gmail.com>
2019-01-11 03:05:03 +0800

    add nginx headers_more update script into cron script

--- helpers_views/helper_nginx_scripts/opt/nginx_scripts/cron_ngx_modules_updates.sh
+++ helpers_views/helper_nginx_scripts/opt/nginx_scripts/cron_ngx_modules_updates.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# nginx headers_more module
+/opt/nginx_scripts/headers_01_headers_more_install.sh
+
+# nginx waf module
 /opt/nginx_scripts/waf_02_nginx_connector_install.sh
 /opt/nginx_scripts/waf_03_owasp_install.sh
