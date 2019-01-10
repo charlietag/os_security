32f62b49b2e1428eb85b198a2f18df48d9910556
Charlie Hsieh <charlieofficial@gmail.com>
2019-01-10 23:57:32 +0800

    adding nginx module more_headers

---
+++ functions/F_02_PKG_06_nginx_03_headers_01_setup.sh
@@ -0,0 +1,25 @@
+# =====================
+# Enable databag
+# =====================
+# RENDER_CP
+
+# =====================
+# Install libmodsecurity (ModSecurity for Nginx) + ModSecurity-nginx-connector
+
+# ******* Install Nginx-modsecurity update script *******
+echo "========================================="
+echo "1. Setup install update Nginx-modsecurity scripts"
+echo "2. Setup Nginx configs for Nginx-WAF"
+echo "========================================="
+task_copy_using_cat
+
+chmod 755 /opt/nginxwaf_scripts/*.sh
+
+# *********************************
+# Adding nginx waf related update scripts into crontab
+# *********************************
+echo "Adding nginx waf related update scripts into crontab..."
+sed -i /waf_/d /etc/crontab
+echo "1 2 * * * root ${connector_install_script}" >> /etc/crontab
+echo "30 2 * * * root ${owasp_install_script}" >> /etc/crontab
+
