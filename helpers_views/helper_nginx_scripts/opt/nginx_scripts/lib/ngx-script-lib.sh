cc3ff57b1068cc3c973e44d8fc1d35aae00a4112
Charlie Hsieh <charlieofficial@gmail.com>
2019-05-08 01:33:10 +0800

    add extended lib: determine if current block is sourced script or function

--- helpers_views/helper_nginx_scripts/opt/nginx_scripts/lib/ngx-script-lib.sh
+++ helpers_views/helper_nginx_scripts/opt/nginx_scripts/lib/ngx-script-lib.sh
@@ -99,3 +99,49 @@ function check_app_run(){
   fi
 }
 
+#-----------------------------------------------------------------------------------------
+# Determine is current block is sourced script or function
+# If yes, please use 'return 0' to skip current block, and continue the whole script
+# Otherwised, please use 'exit 0'!
+# Usage: Use the following command in your script.
+#       eval "${IF_IS_SOURCED_OR_FUNCTION}"
+# Example:
+#         [[ -n "$(eval "${IF_IS_SOURCED_OR_FUNCTION}")" ]] && return 0 || exit 0
+# OR just:
+# eval "${SKIP_SCRIPT}"
+#-----------------------------------------------------------------------------------------
+
+IF_IS_SOURCED_SCRIPT="$(cat <<EOF
+if [[ "\${BASH_SOURCE[0]}" != "\${0}" ]]; then
+  echo "True: use 'return 0' to skip script"
+fi
+EOF
+)"
+
+IF_IS_FUNCTION="$(cat <<EOF
+if [[ -n "\${FUNCNAME}" ]]; then
+  echo "True: use 'return 0' to skip script"
+fi
+EOF
+)"
+
+IF_IS_SOURCED_OR_FUNCTION="$(cat <<EOF
+if [[ -n "\$(eval "\${IF_IS_SOURCED_SCRIPT}")" ]] || [[ -n "\$(eval "\${IF_IS_FUNCTION}")" ]]; then
+  echo "True: use 'return 0' to skip script"
+fi
+EOF
+)"
+
+#SKIP_SCRIPT="$(cat <<EOF
+#[[ -n "\$(eval "\${IF_IS_SOURCED_OR_FUNCTION}")" ]] && return 0 || exit 0
+#EOF
+#)"
+
+SKIP_SCRIPT="$(cat <<EOF
+if [[ -n "\$(eval "\${IF_IS_SOURCED_OR_FUNCTION}")" ]]; then
+  return 0
+else
+  exit 0
+fi
+EOF
+)"
