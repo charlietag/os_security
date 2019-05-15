c9998183f8bea2f0cb5377437ba4a6f032ded57b
Charlie Hsieh <charlieofficial@gmail.com>
2019-05-15 22:01:04 +0800

    move cron update script path into variable

--- helpers/helper_nginx_scripts.sh
+++ helpers/helper_nginx_scripts.sh
@@ -10,6 +10,6 @@ chmod 755 /opt/nginx_scripts/*.sh
 # Adding nginx scripts into crontab
 # *********************************
 echo "Adding nginx related update scripts into crontab..."
-sed -i /cron_ngx_modules_updates/d /etc/crontab
-echo "1 2 * * * root /opt/nginx_scripts/cron_ngx_modules_updates.sh" >> /etc/crontab
+sed -re "/${cron_ngx_modules_updates////\/}/d" -i /etc/crontab
+echo "1 2 * * * root ${cron_ngx_modules_updates}" >> /etc/crontab
 
