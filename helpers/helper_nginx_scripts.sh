0de3fc71886ff6e1296a47148224eb52f00a66f9
Charlie Hsieh <charlieofficial@gmail.com>
2019-05-15 21:08:06 +0800

    using render_sed instead of copy using cat

--- helpers/helper_nginx_scripts.sh
+++ helpers/helper_nginx_scripts.sh
@@ -2,7 +2,7 @@
 echo "========================================="
 echo "Setup Nginx scripts"
 echo "========================================="
-task_copy_helper_view_using_cat
+task_copy_helper_view_using_render_sed
 
 chmod 755 /opt/nginx_scripts/*.sh
 
