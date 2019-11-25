50fc1456de8cefcf84c2caf74ad4398788942328
Charlie Hsieh <charlieofficial@gmail.com>
2019-11-25 15:36:21 +0800

    To make sure script is executable, using cp script first

--- functions/F_02_PKG_08_redmine_01_fail2ban.sh
+++ functions/F_02_PKG_08_redmine_01_fail2ban.sh
@@ -7,6 +7,11 @@
 echo "========================================="
 echo "   Setup fail2ban for redmine (Failed login)"
 echo "========================================="
+
+# To make sure script is executable, using cp script first, this will copy file attr as well
+task_copy_using_cp
+
+# Replace template with variable
 task_copy_using_render_sed
 
 echo "---reload fail2ban---"
