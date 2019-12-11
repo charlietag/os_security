fffc25a1e073164299f8178e5a68f83127626401
Charlie Hsieh <charlieofficial@gmail.com>
2019-12-11 15:07:31 +0800

    tune check_fail2ban script

---
+++ templates/F_03_CHECK_01_check_scripts/opt/check_scripts/scripts/check_fail2ban.sh
@@ -0,0 +1,60 @@
+test_fail2ban_config_status() {
+  local f2b_status="$(fail2ban-client status | tail -n 1 | cut -d':' -f2 | sed "s/\s//g" | tr ',' '\n'|sort -n )"
+  local f2b_config="$(cat /etc/fail2ban/jail.local /etc/fail2ban/${f2b_config_path}jail.d/*.local |grep -E "\[[^[:space:]]+\]$" | grep -v "DEFAULT" | grep -vE "^#" | sed -re 's/^\[//g' | sed -re 's/\]$//g' | sort -n )"
+  local f2b_iptables="$(iptables -S | grep -Eo "f2b-[^[:space:]]+" | sed 's/f2b-//g' | sort -n )"
+  local f2b_ipset="$(ipset list | grep -Eo "f2b-[^[:space:]]+" | sed 's/f2b-//g' | sort -n )"
+
+  local diff_check="$(  diff <(echo "${f2b_status}") <(echo "${f2b_config}")    ; \
+                        diff <(echo "${f2b_status}") <(echo "${f2b_iptables}")  ; \
+                        diff <(echo "${f2b_status}") <(echo "${f2b_ipset}")  
+                    )"
+  if [[ -n "${diff_check}" ]]; then
+    echo "---------------------------"
+    echo "fail2ban config is not loaded correctly!"
+    echo "---------------------------"
+    echo -e "${diff_check}"
+  fi
+}
+
+test_ipset() {
+  local ipset_ban_jail="f2b-${1}"
+  local test_ban_ip="${2}"
+  local ipset_list="$(ipset list ${ipset_ban_jail} | grep "${test_ban_ip}" )"
+  [[ -z "${ipset_list}" ]] && echo -e "ipset \"${ipset_ban_jail}\":\t\t... WARNING"
+}
+
+test_ban() {
+  local test_ban_ip="10.255.255.254"
+  local f2b_jails="$(fail2ban-client status | tail -n 1 | cut -d':' -f2 | sed "s/\s//g" | tr ',' '\n'|sort -n )"
+  local test_ipset_warning
+
+  for f2b_jail in ${f2b_jails[@]}; do
+    test_ipset_warning="$(test_ipset "${f2b_jail}" "${test_ban_ip}")"
+    if [[ -n "${test_ipset_warning}" ]]; then
+      echo "------"
+      echo "${test_ipset_warning}"
+      echo "try to add ip into ipset banning list..."
+      fail2ban-client set ${f2b_jail} banip ${test_ban_ip}
+      echo "------"
+    fi
+    test_ipset_warning="$(test_ipset "${f2b_jail}" "${test_ban_ip}")"
+    if [[ -n "${test_ipset_warning}" ]]; then
+      echo "${test_ipset_warning}"
+      echo "Firewalld is not running correctly!"
+    fi
+  done
+
+
+  #fail2ban-client status | tail -n 1 | cut -d':' -f2 | sed "s/\s//g" | tr ',' '\n' |xargs -i bash -c "echo ---{}---; fail2ban-client set {} banip 10.255.255.254; echo"
+}
+
+check_fail2ban() {
+  local diff_status_msg="$(test_fail2ban_config_status)"
+  local test_ban_msg="$(test_ban)"
+  if [[ -n "${diff_status_msg}" ]] || [[ -n "${test_ban_msg}" ]] ; then
+    display_check_name
+
+    echo -e "${diff_status_msg}"
+    echo -e "${test_ban_msg}"
+  fi
+}
--- templates/F_03_CHECK_01_check_scripts/opt/check_scripts/scripts/check_iptables.sh
+++
@@ -1,8 +0,0 @@
-check_iptables() {
-  local f2b_iptables="$(iptables -S |grep fail2ban)"
-  if [[ -z "${f2b_iptables}" ]]; then
-    display_check_name
-    #echo "${f2b_iptables}"
-    echo "Firewalld is not running correctly!"
-  fi
-}
