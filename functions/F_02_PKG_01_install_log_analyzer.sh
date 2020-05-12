3dd02ac168acd4211811a6df5229279dcea85255
Charlie Hsieh <charlieofficial@gmail.com>
2020-05-12 14:40:43 +0800

    change for CentOS 8

--- functions/F_02_PKG_01_install_log_analyzer.sh
+++ functions/F_02_PKG_01_install_log_analyzer.sh
@@ -12,18 +12,50 @@
 # ***************************
 # logwatch
 # ***************************
-yum install -y logwatch
+dnf install -y logwatch
 
 # ***************************
 # logwatch
 # ***************************
-yum install -y goaccess
+# -- goaccess does not exist in CentOS8 repos (base, appstream, epel) --
+#dnf install -y goaccess
+
+# -- Manually install --
+dnf install -y GeoIP GeoIP-devel
+
+#-----------------------------------------------------------------------------------------
+# Compile and install goaccess
+#-----------------------------------------------------------------------------------------
+cd $TMP
+
+local goaccess_script="/usr/local/bin/goaccess"
+local goaccess_url="https://github.com/allinurl/goaccess/archive/v${goaccess_ver}.tar.gz"
+
+# Download source code
+wget $goaccess_url -O - | tar -xz
+
+# Compile bin file
+cd goaccess-${goaccess_ver}
+autoreconf -fiv
+./configure --enable-utf8 --enable-geoip=legacy
+make
+cp goaccess ${goaccess_script}
+
+# Delete source code
+cd $TMP
+SAFE_DELETE "goaccess-${goaccess_ver}"
+
+
+
+
+
 
 # ***************************
 # Postfix log parse
 # ref. http://www.postfix.org/addon.html#logfile
 # ***************************
-yum install -y postfix-perl-scripts
+# The same function as what logwatch does
+#dnf install -y postfix-perl-scripts
 #sed -i /pflogsumm/d /etc/crontab
 #echo "01 01 * * * root /usr/sbin/pflogsumm -d yesterday /var/log/maillog" >> /etc/crontab
 
