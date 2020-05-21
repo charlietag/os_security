7afb14e126110027e1e595b43da5906ed182e194
Charlie Hsieh <charlieofficial@gmail.com>
2020-05-21 15:51:34 +0800

    fix for compatible with CentOS 8 bash env

--- helpers/helper_nginx_scripts.sh
+++ helpers/helper_nginx_scripts.sh
@@ -10,6 +10,6 @@ chmod 755 /opt/nginx_scripts/*.sh
 # Adding nginx scripts into crontab
 # *********************************
 echo "Adding nginx related update scripts into crontab..."
-sed -re "/${cron_ngx_modules_updates////\/}/d" -i /etc/crontab
+sed -re "/${cron_ngx_modules_updates//\//\\/}/d" -i /etc/crontab
 echo "1 2 * * * root ${cron_ngx_modules_updates}" >> /etc/crontab
 
