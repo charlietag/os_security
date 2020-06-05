a98a2293b643315a0e8f51facd6564cd5e1e75d4
Charlie Hsieh <charlieofficial@gmail.com>
2020-06-05 14:08:02 +0800

    waf rules - use cwaf_charlietag instead , which is a comodo waf rules clone, to avoid unstable comodo waf server

---
+++ helpers_views/helper_nginx_scripts/opt/nginx_scripts/scripts/waf_03_rules_cwaf_charlietag_apply.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# ------------------------------------
+# Source script lib
+# ------------------------------------
+. "$(dirname $(readlink -m $0))/../lib/ngx-script-lib.sh"
+
+# ------------------------------------
+# Only install when you choose to install "COMODO" rules
+# ------------------------------------
+[[ "${PARAM_WAF_RULES}" != "cwaf_charlietag" ]] && eval "${SKIP_SCRIPT}"
+
+# ------------------------------------
+# Define and check app version
+# ------------------------------------
+# App version
+# PARAM_COMODO_RULES_VER : defined in cfg file
+
+# Check app version
+check_app "cwaf-rules-version" "${PARAM_COMODO_RULES_VER}"
+check_app_run  # Comment this line, to avoid check app version , always execute this script
+
+# ------------------------------------
+
+start_script
+# ***********************************************************************************************************
+# ------------------------------------
+# Start
+# ------------------------------------
+echo " ------------------------------------"
+echo " Install COMODO Rules - CWAF...."
+echo " ------------------------------------"
+# Install Comodo WAF Rules....
+
+# Start to setup comodo rules
+wget \
+--content-disposition \
+https://github.com/charlietag/cwaf_charlietag/raw/master/output/cwaf_rules_nginx_3-${PARAM_COMODO_RULES_VER}.tgz \
+ -O - | tar -xz
+
+IF_RULES_DOWNLOADED="$(ls ${THIS_PATH_TMP} | grep '\.conf')"
+if [[ -n "${IF_RULES_DOWNLOADED}" ]]; then
+  echo 
+  echo ">>>>>>>>>>>>>>>"
+  echo "SAFE_DELETE \"${PARAM_COMODO_RULES_PATH}/*\" ....!"
+  echo 
+  SAFE_DELETE "${PARAM_COMODO_RULES_PATH}/*"
+
+  echo 
+  echo ">>>>>>>>>>>>>>>"
+  echo "Copy comodo rules ---> ${PARAM_COMODO_RULES_PATH}/ ....!"
+  cd ${PARAM_COMODO_RULES_PATH}
+  echo 
+  \cp -f ${THIS_PATH_TMP}/* ${PARAM_COMODO_RULES_PATH}/
+fi
+
+echo "Done ...!"
+echo 
+
+# ***********************************************************************************************************
+
+# ------------------------------------
+# Stop
+# ------------------------------------
+stop_script
