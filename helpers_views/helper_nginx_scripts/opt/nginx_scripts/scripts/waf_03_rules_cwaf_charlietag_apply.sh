130a9756f2c763ce9e01e828219fe209b64ce47f
Charlie Hsieh <charlieofficial@gmail.com>
2020-06-05 14:41:12 +0800

    cwaf_charlietag uses its own var name

--- helpers_views/helper_nginx_scripts/opt/nginx_scripts/scripts/waf_03_rules_cwaf_charlietag_apply.sh
+++ helpers_views/helper_nginx_scripts/opt/nginx_scripts/scripts/waf_03_rules_cwaf_charlietag_apply.sh
@@ -6,7 +6,7 @@
 . "$(dirname $(readlink -m $0))/../lib/ngx-script-lib.sh"
 
 # ------------------------------------
-# Only install when you choose to install "COMODO" rules
+# Only install when you choose to install "cwaf_charlietag" rules
 # ------------------------------------
 [[ "${PARAM_WAF_RULES}" != "cwaf_charlietag" ]] && eval "${SKIP_SCRIPT}"
 
@@ -14,10 +14,10 @@
 # Define and check app version
 # ------------------------------------
 # App version
-# PARAM_COMODO_RULES_VER : defined in cfg file
+# PARAM_CWAF_RULES_VER : defined in cfg file
 
 # Check app version
-check_app "cwaf-rules-version" "${PARAM_COMODO_RULES_VER}"
+check_app "cwaf-rules-version" "${PARAM_CWAF_RULES_VER}"
 check_app_run  # Comment this line, to avoid check app version , always execute this script
 
 # ------------------------------------
@@ -35,23 +35,23 @@ echo " ------------------------------------"
 # Start to setup comodo rules
 wget \
 --content-disposition \
-https://github.com/charlietag/cwaf_charlietag/raw/master/output/cwaf_rules_nginx_3-${PARAM_COMODO_RULES_VER}.tgz \
+https://github.com/charlietag/cwaf_charlietag/raw/master/output/cwaf_rules_nginx_3-${PARAM_CWAF_RULES_VER}.tgz \
  -O - | tar -xz
 
 IF_RULES_DOWNLOADED="$(ls ${THIS_PATH_TMP} | grep '\.conf')"
 if [[ -n "${IF_RULES_DOWNLOADED}" ]]; then
   echo 
   echo ">>>>>>>>>>>>>>>"
-  echo "SAFE_DELETE \"${PARAM_COMODO_RULES_PATH}/*\" ....!"
+  echo "SAFE_DELETE \"${PARAM_CWAF_CHARLIETAG_RULES_PATH}/*\" ....!"
   echo 
-  SAFE_DELETE "${PARAM_COMODO_RULES_PATH}/*"
+  SAFE_DELETE "${PARAM_CWAF_CHARLIETAG_RULES_PATH}/*"
 
   echo 
   echo ">>>>>>>>>>>>>>>"
-  echo "Copy comodo rules ---> ${PARAM_COMODO_RULES_PATH}/ ....!"
-  cd ${PARAM_COMODO_RULES_PATH}
+  echo "Copy comodo rules ---> ${PARAM_CWAF_CHARLIETAG_RULES_PATH}/ ....!"
+  cd ${PARAM_CWAF_CHARLIETAG_RULES_PATH}
   echo 
-  \cp -f ${THIS_PATH_TMP}/* ${PARAM_COMODO_RULES_PATH}/
+  \cp -f ${THIS_PATH_TMP}/* ${PARAM_CWAF_CHARLIETAG_RULES_PATH}/
 fi
 
 echo "Done ...!"
